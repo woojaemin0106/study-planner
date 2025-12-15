dbb136cae9ca01ea86472a41964caf285949b2ba
woojaemin0106 <dnwoals19@naver.com>
2025-12-15 19:01:05 +0900

    feat: pages분리 + MainPage 구현

---
+++ src/pages/ChallengesPage.jsx
@@ -0,0 +1,3 @@
+export default function ChallengesPage() {
+  return <div className="p-6">Challenges</div>;
+}
---
+++ src/pages/DaysPage.jsx
@@ -0,0 +1,3 @@
+export default function DaysPage() {
+  return <div className="p-6">Days</div>;
+}
