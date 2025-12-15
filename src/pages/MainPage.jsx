c823b73fdd60b6e5c3e60f7fd9ea915cc2e3dd57
user <oprenber@gmail.com>
2025-12-15 19:20:42 +0900

    feat:NavBar 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -32,7 +32,7 @@ export default function MainPage() {
   return (
     <div className="w-full">
       {/* Hero */}
-      <section className="w-full bg-gray-200">
+      <section className="w-full ">
         <div className="mx-auto max-w-6xl px-6 py-20">
           <div className="flex flex-col items-center justify-center text-center">
             <h1 className="text-3xl font-extrabold tracking-tight md:text-4xl">
