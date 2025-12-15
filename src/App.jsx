9fb33601e5762e3c5e2dbe248a5e3968ba51a9a8
woojaemin0106 <dnwoals19@naver.com>
2025-12-15 19:01:05 +0900

    feat: pages분리 + MainPage 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from './components/NavBar.jsx'
+import { Routes, Route, Navigate } from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
 
-function App() {
-    return (
-        <div>
-            <Navbar />
-            <Outlet />
-        </div>
-    )
+import MainPage from "./pages/MainPage.jsx";
+import DaysPage from "./pages/DaysPage.jsx";
+import TimerPage from "./pages/TimerPage.jsx";
+import ChallengesPage from "./pages/ChallengesPage.jsx";
+
+function LoginPage() {
+  return <div className="p-6">Login</div>;
+}
+function SignupPage() {
+  return <div className="p-6">Signup</div>;
 }
 
-export default App
+export default function App() {
+  return (
+    <>
+      <NavBar />
+
+      <Routes>
+        {/* Home */}
+        <Route path="/" element={<MainPage />} />
+
+        {/* Tabs */}
+        <Route path="/Days" element={<DaysPage />} />
+        <Route path="/Timer" element={<TimerPage />} />
+        <Route path="/Challenges" element={<ChallengesPage />} />
+
+        {/* Auth */}
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Signup" element={<SignupPage />} />
+
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
+  );
+}
---
+++ src/pages/TimerPage.jsx
@@ -0,0 +1,3 @@
+export default function TimerPage() {
+  return <div className="p-6">Timer</div>;
+}
