360c94698c0cf659c45f74a3ec76804f54c0b2d4
woojaemin0106 <dnwoals19@naver.com>
2025-12-17 18:37:59 +0900

    feat:Days (4개) 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import NavBar from "./components/NavBar.jsx";
 import MainPage from "./pages/MainPage.jsx";
-import DaysPage from "./pages/DaysPage.jsx";
 import TimerPage from "./pages/TimerPage.jsx";
 import ChallengesPage from "./pages/ChallengesPage.jsx";
 import DaysLayout from "./pages/days/DaysLayout.jsx";
-import DaysDailyPage from "./pages/days/DaysDailyPage.jsx";
-import DaysWeeklyPage from "./pages/days/DaysWeeklyPage.jsx";
-import DaysMonthlyPage from "./pages/days/DaysMonthlyPage.jsx";
 
 function LoginPage() {
   return <div className="p-6">Login</div>;
@@ -26,7 +22,13 @@ export default function App() {
         <Route path="/" element={<MainPage />} />
 
         {/* Tabs */}
-        <Route path="/Days" element={<DaysPage />} />
+        <Route path="/Days" element={<DaysLayout />}>
+          <Route index element={<Navigate to="daily" replace />} />
+          <Route path="daily" element={<DaysDailyPage />} />
+          <Route path="weekly" element={<DaysWeeklyPage />} />
+          <Route path="monthly" element={<DaysMonthlyPage />} />
+        </Route>
+
         <Route path="/Timer" element={<TimerPage />} />
         <Route path="/Challenges" element={<ChallengesPage />} />
 
